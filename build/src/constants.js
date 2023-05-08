4e1589e4e9985d29449c9bbd3452a3dc3ea4c431
Dan0xE <danopperm@gmail.com>
2023-05-08 15:34:28 +0200

    removed build folder

--- build/src/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const isDev = process.env.NODE_ENV === 'development';
-exports.default = isDev;
-//# sourceMappingURL=constants.js.map
--- build/src/enums.d.ts
+++
@@ -1,8 +0,0 @@
-declare enum LogLevel {
-    DEBUG = 0,
-    INFO = 1,
-    WARN = 2,
-    ERROR = 3,
-    FATAL = 4
-}
-export default LogLevel;
